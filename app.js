3e7ee5a315d1584feb24f5f1cd6389e623952ff4
Luan Arend <108678642+luan-arend@users.noreply.github.com>
2023-01-11 11:27:59 -0300

    Atualizando código (antes de criar variáveis para as moedas)

--- app.js
+++ app.js
@@ -1,56 +1,21 @@
 let coins = [
-    {
-        name: "R$ 0.01",
-        value: 0.01 ,
-        stock: 10
-    },
-    {
-        name: "R$ 0.05",
-        value: 0.05 ,
-        stock: 10
-    },
-    {
-        name: "R$ 0.10",
-        value: 0.10 ,
-        stock: 10
-    },
-    {
-        name: "R$ 0.25",
-        value: 0.25 ,
-        stock: 10
-    },
-    {
-        name: "R$ 0.50",
-        value: 0.50 ,
-        stock: 10
-    },
-    {
-        name: "R$ 1.00",
-        value: 1.00 ,
-        stock: 10
-    }
+    {name: "R$ 0.01", value: 0.01, stock: 10},
+    {name: "R$ 0.05", value: 0.05, stock: 10},
+    {name: "R$ 0.10", value: 0.10, stock: 10},
+    {name: "R$ 0.25", value: 0.25, stock: 10},
+    {name: "R$ 0.50", value: 0.50, stock: 10},
+    {name: "R$ 1.00", value: 1.00, stock: 10}
 ]
 
 let products = [
-    {
-        name: "coca-cola",
-        price: 1.50 ,
-        stock: 10
-    },
-    {
-        name: "agua",
-        price: 1.00 ,
-        stock: 10
-    },
-    {
-        name: "pastelina",
-        price: 0.30 ,
-        stock: 10
-    }
+    {name: "coca-cola", price: 1.50, stock: 10},
+    {name: "agua", price: 1.00, stock: 10},
+    {name: "pastelina", price: 0.30, stock: 10}
 ]
 
 let saldo = 0
 saldo = parseFloat(saldo)
+let listaDeProdutos = []
 
 function exibirMenu(){
     return prompt(
@@ -79,7 +44,7 @@ function executar(){
     }
 }
 
-function verificaMoedas(value) {
+function verificarMoedas(value) {
     let index = coins.findIndex(val => val.value == value)
         if (index >= 0) {
             return true
@@ -89,10 +54,15 @@ function verificaMoedas(value) {
 
 function adicionarMoedas(value) {
     let moeda1 = parseFloat ( prompt ("\nInsira uma das moedas válidas abaixo:" + "\nR$0.01, R$0.05, R$0.10, R$0.25, R$0.50, R$1.00"))
-        if (verificaMoedas(moeda1)){
+        if (verificarMoedas(moeda1)){
             saldo += moeda1
             alert(`Você inseriu a moeda: R$ ${moeda1}`)
+            return alert()
         } else {
             alert("Moeda inválida, tente novamente.")
             }
+}
+
+function atualizarStockMoedas(){
+    
 }
